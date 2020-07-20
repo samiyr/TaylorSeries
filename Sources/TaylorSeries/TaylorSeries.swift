666da20b34a26f8d8aebbf7216b048217950dbdc
Sami Yrjänheikki <sami.yr@protonmail.com>
2020-07-20 13:57:05 +0300

    Adopt swift-numerics

--- Sources/TaylorSeries/TaylorSeries.swift
+++ Sources/TaylorSeries/TaylorSeries.swift
@@ -1,28 +1,29 @@
 import Foundation
+import Numerics
 
 /// Represents a Taylor Series expansion for some real-analytic function.
-public struct TaylorSeries {
+public struct TaylorSeries<Number: Real> {
     /// A closure implementing the terms to be summed.
     /// `x` includes both the computation point and the center, meaning it's really `(x - x_0)`.
     /// `n` is the summation index, starting at `start`.
     /// See `Common` for examples on how it works.
-    public typealias Summand = (_ x: Double, _ n: Int) -> (Double)
+    public typealias Summand = (_ x: Number, _ n: Int) -> (Number)
     /// Terms to be summed. This must include everything in one package, including the (x - x_0)^n and n! parts. The reason for this is that it allows some sums (like sin and cos) to be defined more easily.
     public var summand: Summand
     /// Start index of the sum. Defaults to 0.
     public var start: Int = 0
     
     /// Returns a callable closure with the series centered at `center` and truncated at `up`.
-    public func truncatedSeries(center: Double, up to: Int) -> (Double) -> (Double) {
+    public func truncatedSeries(center: Number, up to: Int) -> (Number) -> (Number) {
         return { x in
             return (start...to).map { summand(x - center, $0) }.reduce(0, +)
         }
     }
     /// Returns a callable closure where the series is truncated at an appropariate point such that the difference between the last two terms is less than `epsilon`. However, the summing is capped by `max`, which defaults to 1000.
-    public func truncatedSeries(center: Double, to epsilon: Double, max iterations: Int = 1000) -> (Double) -> (Double) {
+    public func truncatedSeries(center: Number, to epsilon: Number, max iterations: Int = 1000) -> (Number) -> (Number) {
         return { x in
-            var value = 0.0
-            var prev = 0.0
+            var value = Number(0)
+            var prev = Number(0)
             var i = start
             repeat {
                 if i > iterations { break }
@@ -37,38 +38,57 @@ public struct TaylorSeries {
     /// Defines some common series terms for ease of use.
     public struct Common {
         /// 1 / (1 - x)
-        public static let geometric: Summand = { x, n in
-            return pow(x, Double(n))
+        public static var geometric: Summand {
+            return { x, n in
+                return Number.pow(x, n)
+            }
         }
-        public static let exp: Summand = { x, n in
-            return pow(x, Double(n)) / factorial(n)
+        
+        public static var exp: Summand {
+            return { x, n in
+                return Number.pow(x, n) / factorial(n)
+            }
         }
-        public static let sin: Summand = { x, n in
-            return paritySign(n) * pow(x, Double(2 * n + 1)) / factorial(2 * n + 1)
+        public static var sin: Summand {
+            return { x, n in
+                return paritySign(n) * Number.pow(x, 2 * n + 1) / factorial(2 * n + 1)
+            }
         }
-        public static let sinh: Summand = { x, n in
-            return pow(x, Double(2 * n + 1)) / factorial(2 * n + 1)
+        public static var sinh: Summand {
+            return { x, n in
+                return Number.pow(x, 2 * n + 1) / factorial(2 * n + 1)
+            }
         }
-        public static let arcsin: Summand = { x, n in
-            let num = factorial(2 * n) * pow(x, Double(2 * n + 1))
-            let den = pow(2, Double(2 * n)) * pow(factorial(n), 2) * Double(2 * n + 1)
-            return num / den
+        public static var arcsin: Summand {
+            return { x, n in
+                let num = factorial(2 * n) * Number.pow(x, 2 * n + 1)
+                let den = Number.pow(2, 2 * n) * Number.pow(factorial(n), 2) * Number(2 * n + 1)
+                return num / den
+            }
         }
-        public static let cos: Summand = { x, n in
-            return paritySign(n) * pow(x, Double(2 * n)) / factorial(2 * n)
+        public static var cos: Summand {
+            return { x, n in
+                return paritySign(n) * Number.pow(x, 2 * n) / factorial(2 * n)
+            }
         }
-        public static let cosh: Summand = { x, n in
-            return pow(x, Double(2 * n)) / factorial(2 * n)
+        public static var cosh: Summand {
+            return { x, n in
+                return Number.pow(x, 2 * n) / factorial(2 * n)
+            }
         }
-        public static let arctan: Summand = { x, n in
-            // Workaround to 'compiler is unable to type-check the expression in reasonable time'
-            let num = paritySign(n) * pow(x, Double(2 * n + 1))
-            let den = Double(2 * n + 1)
-            return num / den
+        public static var arctan: Summand {
+            return { x, n in
+                // Workaround to 'compiler is unable to type-check the expression in reasonable time'
+                let num = paritySign(n) * Number.pow(x, 2 * n + 1)
+                let den = Number(2 * n + 1)
+                return num / den
+            }
         }
-        public static let logPlusOne: Summand = { x, n in
-            if n == 0 { return 0 }
-            return paritySign(n + 1) * pow(x, Double(n)) / Double(n)
+        public static var logPlusOne: Summand {
+            return { x, n in
+                if n == 0 { return 0 }
+                return paritySign(n + 1) * Number.pow(x, n) / Number(n)
+            }
         }
     }
     
@@ -76,13 +96,12 @@ public struct TaylorSeries {
     
     /// Factorial, n! = n * (n-1) * (n-2) * ... * 2 * 1.
     /// 0! = 1! = 1
-    public static func factorial(_ n: Int) -> Double {
-        if n == 0 || n == 1 { return 1 }
-        return (1...n).map(Double.init).reduce(1.0, *)
+    public static func factorial(_ n: Int) -> Number {
+        return Number.gamma(Number(n + 1))
     }
 
-    /// Gives (-1)^n as a `Double`.
-    public static func paritySign(_ n: Int) -> Double {
+    /// Gives (-1)^n as a `Number`.
+    public static func paritySign(_ n: Int) -> Number {
         if n % 2 == 0 {
             return 1
         } else {
--- Tests/TaylorSeriesTests/TaylorSeriesTests.swift
+++ Tests/TaylorSeriesTests/TaylorSeriesTests.swift
@@ -5,16 +5,52 @@ import Darwin
 
 final class TaylorSeriesTests: XCTestCase {
     func testExp() {
-        let series = TaylorSeries(summand: TaylorSeries.Common.exp)
+        let series = TaylorSeries<Double>(summand: TaylorSeries.Common.exp)
         let exp = series.truncatedSeries(center: 0, up: 20)
         XCTAssert(abs(exp(1.0) - Darwin.M_E) < 1e-6)
     }
     func testSin() {
-        let series = TaylorSeries(summand: TaylorSeries.Common.sin)
+        let series = TaylorSeries<Double>(summand: TaylorSeries.Common.sin)
         let sin = series.truncatedSeries(center: 0, to: 1e-16)
         XCTAssert(abs(sin(1.0) - Darwin.sin(1.0)) < 1e-16)
     }
 
+    func testFactorialPerformance() {
+        measure {
+            for _ in 0...10000 {
+                let _ = (1...10).map(Double.init).reduce(1, *)
+            }
+        }
+    }
+    func testGammaPerformance() {
+        measure {
+            for _ in 0...10000 {
+                let _ = Double.gamma(11)
+            }
+        }
+    }
+    func testParitySignPerformance1() {
+        func parity(_ n: Int) -> Double {
+            if n % 2 == 0 {
+                return 1
+            } else {
+                return -1
+            }
+        }
+        measure {
+            for i in 0...1000000 {
+                let _ = parity(i)
+            }
+        }
+    }
+    func testParitySignPerformance2() {
+        measure {
+            for i in 0...1000000 {
+                let _ = Double.pow(-1, i)
+            }
+        }
+    }
+    
     static var allTests = [
         ("expTest", testExp),
         ("sinTest", testSin)
